a4166a95017128850c03aba0693df5e2fea63bad
PhuDo1709 <phudothanh@gmail.com>
2025-04-23 12:51:47 +1000

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/Card";
 import { Button } from "@/components/ui/button";
 import { FiTrendingUp, FiHome, FiBriefcase, FiDollarSign, FiSettings } from "lucide-react";
 
