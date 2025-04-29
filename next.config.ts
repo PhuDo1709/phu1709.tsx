e2dad555337f413b32d64542c11f055b6f0740cf
PhuDo1709 <phudothanh@gmail.com>
2025-04-29 10:10:15 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Ensures better error handling and warnings during development
+  typescript: {
+    ignoreBuildErrors: false,  // Change to 'true' if you want to ignore type errors during build
+  },
 };
 
 export default nextConfig;
