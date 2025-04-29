501b87590ffa7934eec35751b8f3a5369d6b3a16
PhuDo1709 <phudothanh@gmail.com>
2025-04-30 08:27:55 +1000

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,12 +1,11 @@
-// app/page.tsx
-
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
     <div className={styles.container}>
       <h1>Welcome to No Worry App</h1>
-      <p>Invest smart, raise your future.</p>
+      <p>Grow your money. Raise your children.</p>
     </div>
   );
 }
