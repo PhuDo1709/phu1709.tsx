2459d4f32c10d3a5d94c0af353fbabf9f1679253
Your Name <your-email@example.com>
2025-05-30 11:49:46 +1000

    Fix type for children in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
-import Sidebar from '@/components/sidebar';
-import Topbar from '@/components/topbar';
+import Topbar from '@/components/topbar'
+import Sidebar from '@/components/sidebar'
+import { ReactNode } from 'react'
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="flex">
         <Sidebar />
-        <main className="flex-1">
+        <div className="flex-1">
           <Topbar />
           {children}
-        </main>
+        </div>
       </body>
     </html>
-  );
+  )
 }
+
 
 
 
